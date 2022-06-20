27455aace7cab938bd95be07815760fd410c34a2
maksonchik20 <99185340+maksonchik20@users.noreply.github.com>
2022-06-20 18:33:18 +0300

    Update settings.py

--- test_django/settings.py
+++ test_django/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maksonchik505@gmail.com'
-EMAIL_HOST_PASSWORD = 'nualeeafxozzpzzw'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True # Возможно убрать
 LOGIN_REDIRECT_URL = 'blog-home'
 
